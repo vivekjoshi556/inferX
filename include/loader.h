da970b0405bc9aaf06e02ec499577ae40c04a155
Vivek Joshi <37882929+vivekjoshi556@users.noreply.github.com>
2026-06-18 22:54:31 +0530

    refactors: Refactor load_model and fix: Fix multi_handle leakage (#5)
    * refactor: extracted hf_model_ls from load_model
    * fix: multi_handle leakage

--- include/loader.h
+++ include/loader.h
@@ -1,12 +1,14 @@
 #ifndef MODEL_LOADER_H
 #define MODEL_LOADER_H
 
+#include <vector>
 #include <iostream>
 #include <filesystem>
 
 namespace fs = std::filesystem;
 
 namespace inferX {
+std::vector<std::string> hf_model_ls(std::string&, std::string&);
 fs::path get_download_dir(std::string& model_id);
 void load_model(std::string model_id, std::string revision = "main",
                 std::string auth_token = "");
